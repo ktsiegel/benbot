7648d53328abd3c6681382d06d5ef856e241ee70
Kate Yu <katexyu@gmail.com>
2023-03-19 18:20:07 -0400

    Add stroke arg

--- server.py
+++ server.py
@@ -2,9 +2,10 @@ import os
 import openai
 
 from dotenv import load_dotenv
+from enum import Enum
 from functools import cache
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from glob import glob
 from os.path import join, abspath
 
@@ -13,6 +14,18 @@ app = Flask(__name__)
 # Initialize OpenAI
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+class Stroke(Enum):
+    BACKSTROKE = 1
+    BREASTSTROKE = 2
+    BUTTERFLY = 3
+    FREESTYLE = 4
+    IM = 5
+    KICKING = 6
+    PULLING = 7
+
+SUPPORTED_STROKES = {s.name for s in Stroke}
+
 
 def make_practice_corpus():
     full_path = join("example_practices", "*.txt")
@@ -28,6 +41,8 @@ EXAMPLE_PRACTICES = make_practice_corpus()
 
 @app.route('/practice', methods=['GET'])
 def practice():
+    stroke_arg = request.args.get('stroke', default = None)
+
     answer = openai.ChatCompletion.create(
         model="gpt-4",
         messages=[
@@ -35,7 +50,7 @@ def practice():
             "Practices must be between 4000 and 5000 yards, and must include a warmup of 600-800 yards, " \
                 "a pre-main set of 600-1200 yards, a main set of 1500-2500 yards, and a cool down of 200-400 yards."},
             {"role": "system", "content": "Here are some example practices:\n" + EXAMPLE_PRACTICES},
-            {"role": "user", "content": "Generate a new swim practice."},
+            {"role": "user", "content": generate_prompt(stroke_arg)},
         ]
     )
 
@@ -43,6 +58,16 @@ def practice():
         'practiceLines': answer["choices"][0]["message"]["content"].split("\n")
     }
     return jsonify(data)
+
+
+def generate_prompt(stroke_arg):
+    prompt = "Generate a new swim practice"
+    if stroke_arg is not None:
+        if stroke_arg.upper() not in SUPPORTED_STROKES:
+            raise f"Unsupported stroke passed in: {stroke_arg}"
+        prompt += f" with a focus on {stroke_arg}"
+    prompt += "."
+    return prompt
 
 
 if __name__ == '__main__':
